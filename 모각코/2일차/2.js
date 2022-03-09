96f1c2830ff6c3c07d32d29cc8bcf61acfa945ed
qkrsh <qkrtkdgus@gachon.ac.kr>
2022-03-09 11:42:15 +0900

    3day commit

---
+++ 모각코/2일차/2.js
@@ -0,0 +1,11 @@
+function changeText1(){
+    document.getElementById("f").innerHTML="모자른 사람이 어딨어요~"
+}
+
+function changeText2(){
+    document.getElementById("s").innerHTML="각자 최선을 다하는거죠!"
+}
+
+function changeText3(){
+    document.getElementById("c").innerHTML="코로나 끝나고 모두 만나요~"  
+}
