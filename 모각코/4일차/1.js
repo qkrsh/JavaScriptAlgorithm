78d59c95bcbdf0612b15f0232b276fae0dea3402
qkrsh <qkrtkdgus@gachon.ac.kr>
2022-03-11 13:19:19 +0900

    add 4day

---
+++ 모각코/4일차/1.js
@@ -0,0 +1,9 @@
+const startButton = document.querySelector(".start");
+
+function start(){
+    for(var a=0;a<7;a++){
+    console.log(a);
+    }
+}
+
+startButton.addEventListener('click', start());
