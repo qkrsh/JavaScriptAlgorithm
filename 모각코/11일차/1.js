c4bb6f15207ffee6ed322cfa63dee9ce6662a650
qkrsh <qkrtkdgus@gachon.ac.kr>
2022-03-23 09:35:27 +0900

    add 12day

--- 모각코/11일차/1.js
+++ 모각코/11일차/1.js
@@ -6,31 +6,50 @@ const hh=document.querySelector('h2');
 
 bt.addEventListener('click',()=>{
     if(cs.value=='불꺼줘'){
+        hh.style.color="rgb(255,255,255)";
         hh.innerHTML="불을 끕니다";
         bd.classList.add('visible');
+        cs.value='';
     }
     else if(cs.value=='불켜줘'){
+        hh.style.color="rgb(255,255,255)";
+
         hh.innerHTML="불을 켭니다";
         bd.classList.remove('visible');
+        cs.value='';
     }
     else if(cs.value=='노래불러줘'){
-        hh.innerHTML="니냐니노니녀닌닌니냐니노니냐니노아아아다시그때로~~니냐니노니냐노닌";
-        setInterval(col,500);
+        hh.innerHTML="니냐니노니녀닌닌니나누나누냐니노니냐니노아아아다시그때로~~니냐니노니냐노닌";
+      
+        let as= setInterval(function(){
+        var a=Math.round(Math.random()*255);
+        var b=Math.round(Math.random()*255);
+        var c=Math.round(Math.random()*255);
+        hh.style.color = "rgb(" + a + "," + b + "," + c + ")";
+        },500)
+        let st= setTimeout(function(){
+            clearInterval(as)
+            },5000)
+        cs.value='';
     }
-    else if(cs.value=='그만'){
-        clearInterval(col);
-        say.style.color="black"
-        hh.innerHTML="왈왈~";
+    else if(cs.value=='춤추자'){
+        hh.innerHTML="ㅇ<ㅡ<~~ㅇ>ㅡ<ㅇ<ㅡ<~~ㅇ>ㅡ<ㅇ<ㅡ<~~ㅇ>ㅡ<ㅇ<ㅡ<~~ㅇ>ㅡ<ㅇ<ㅡ<~~ㅇ>ㅡ<ㅇ<ㅡ<~~ㅇ>ㅡ<ㅇ<ㅡ<~~ㅇ>ㅡ<ㅇ<ㅡ<~~ㅇ>";
+        let sa= setInterval(function(){
+        var a=Math.round(Math.random()*255);
+        var b=Math.round(Math.random()*255);
+        var c=Math.round(Math.random()*255);
+        bd.style.backgroundColor = "rgb(" + a + "," + b + "," + c + ")";
+        },500)
+        let st= setTimeout(function(){
+            clearInterval(sa)
+            },5000)
+        cs.value='';
     }
     else if('경고'){
+        hh.style.color="rgb(255,255,255)";
         hh.innerHTML="왈왈~";
         alert("못알아듣겠어유,,");
+        cs.value='';
     }
 })
 
-function col(){
-    var a=Math.round(Math.random()*255);
-    var b=Math.round(Math.random()*255);
-    var c=Math.round(Math.random()*255);
-    say.style.color = "rgb(" + a + "," + b + "," + c + ")";
-}
