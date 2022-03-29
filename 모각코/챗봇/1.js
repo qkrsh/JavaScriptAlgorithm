c3361d42c97b843173bc5a61d6ce9ad5dbc77d2d
qkrsh <qkrtkdgus@gachon.ac.kr>
2022-03-29 11:53:34 +0900

    update chatbot

--- 모각코/챗봇/1.js
+++ 모각코/챗봇/1.js
@@ -5,6 +5,61 @@ const bd=document.querySelector('body');
 const hh=document.querySelector('h2');
 var nar = document.getElementById("typed");
 
+var json = 
+[
+	{
+		"question" : "안녕",
+		"answer" : "안냥!"
+	},
+	{
+		"question" : "나이",
+		"answer" : "비밀이다!"
+	},
+	{
+		"question" : "이름",
+		"answer" : "하루~~!"
+	}
+]
+
+var question = ""; //사용자의 질문을 임시 저장할 변수
+var answer = ""; //사용자의 대답을 임시 저장할 변수
+var key=0;
+
+
+function text_check(){
+    var value=cs.value;
+	
+	hh.innerHTML = "이해 못했는데 말을 가르쳐 주실래요?(네 or 아니요)";
+    if(value=="네"){
+        question = value; //사용자의 질문을 미리 저장
+        hh.innerHTML = "방금 말에 대한 원하는 답을 입력해주세요!";
+        key = 2;
+    }else{
+        return;
+    }
+    
+    if(key == 2){
+        answer = value; //전역변수 answer값에 사용자의 입력을 저장
+        push_json(); //json 데이터에 값을 추가하는 함수 (추후 생성)
+    }
+    
+    for(let i = 0; i < json.length; i++){
+        if(value == json[i].question){
+            hh.innerHTML = json[i].answer;
+            return;
+        }
+    }
+    cs.value='';
+}
+
+function push_json(){
+    var reply=say;
+    json.push({question: `${question}`, answer: `${answer}`});
+    reply.innerHTML = "하루가 말을 배웠다!";
+	key = 0; //키 값 0으로 초기화
+}
+
+
 
 
 bt.addEventListener('click',()=>{
@@ -70,22 +125,7 @@ bt.addEventListener('click',()=>{
             .start();
     }
     else{
-        var bb=cs.value;
-        hh.style.color="rgb(255,255,255)";
-        hh.innerHTML=bb+"이 뭔지 못알아듣겠어유,,,,~_~";
+        text_check();
         cs.value='';
-        var typewriter = new Typewriter(nar, {
-            loop: true
-        });
-
-        typewriter
-            .typeString("알아듣게 얘기해봐유,,")
-            .pauseFor(1500)
-            .deleteAll()
-            .typeString("모르겠으면 밑에 help에 손 올려봐유,,")
-            .pauseFor(3000)
-            .start();
-        
     }
 })
-
